9def7fabdbb552726c67ffbddcd4615425a95c1f
Gabriela Pimentel <78750796+gabifp@users.noreply.github.com>
2024-12-02 09:02:09 -0300

    feat: add repository access methods to the service

--- src/main/java/com/picpaysimplificado/services/UserService.java
+++ src/main/java/com/picpaysimplificado/services/UserService.java
@@ -22,4 +22,14 @@ public class UserService {
             throw new Exception("Saldo insuficiente");
         }
     }
+
+    public User findUserById(Long id) throws Exception {
+        return this.repository.findUserById(id).orElseThrow(() -> new Exception(
+                "Usuário não encontrado"
+        ));
+    }
+
+    public void saveUser(User user) throws Exception {
+        this.repository.save(user);
+    }
 }
